b7c94ca83dd157ac0a1c446d40d79bb4b5c1768c
Martin Algesten <martin@algesten.se>
2010-11-05 15:07:46 +0100

    Copied in modified YUI compressor for CSS compression.

--- src/main/java/fnug/CssCompressor.java
+++ src/main/java/fnug/CssCompressor.java
@@ -1,9 +1,37 @@
 package fnug;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+import andyr.compressor.AndyRYUICssCompressor;
+
 public class CssCompressor {
 
     public byte[] compress(byte[] input) {
-        return input;
-    }
 
+        StringReader reader = null;
+        try {
+            reader = new StringReader(new String(input, "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            // unlikely.
+        }
+
+        StringWriter writer = new StringWriter();
+
+        try {
+            AndyRYUICssCompressor cmp = new AndyRYUICssCompressor(reader);
+            cmp.compress(writer, 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return writer.toString().getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
