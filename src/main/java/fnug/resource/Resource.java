333d2f3c69229dcdac5917cb1e01fce2156b5922
Martin Algesten <martin@algesten.se>
2010-11-13 20:13:55 +0100

    ASL 2.0 license

--- src/main/java/fnug/resource/Resource.java
+++ src/main/java/fnug/resource/Resource.java
@@ -6,6 +6,22 @@ import javax.activation.FileTypeMap;
 
 import fnug.config.BundleConfig;
 
+/*
+ Copyright 2010 Martin Algesten
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 /**
  * Abstraction of a file with a name, bytes and last modified date. Resources
  * can either be a direct representation of a file or be produced by name other
