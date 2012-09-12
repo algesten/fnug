4bd6da9eadba68a68aaf2c4ba00eb5c61f130aef
Martin Algesten <martin@algesten.se>
2012-09-12 08:01:51 +0200

    removed bad server code

--- src/main/resources/fnug/fnug/dustjs-9015c2c-full.js
+++ src/main/resources/fnug/fnug/dustjs-9015c2c-full.js
@@ -3686,21 +3686,4 @@ var parser = (function(){
 
 dust.parse = parser.parse;
 
-})(typeof exports !== 'undefined' ? exports : getGlobal());var path = require('path'),
-    parser = require('./parser'),
-    compiler = require('./compiler'),
-    vm = require('vm');
-
-module.exports = function(dust) {
-  compiler.parse = parser.parse;
-  dust.compile = compiler.compile;
-
-  dust.loadSource = function(source, path) {
-    return vm.runInNewContext(source, {dust: dust}, path);
-  };
-
-  dust.nextTick = process.nextTick;
-
-  // expose optimizers in commonjs env too
-  dust.optimizers = compiler.optimizers;
-}
+})(typeof exports !== 'undefined' ? exports : getGlobal());
